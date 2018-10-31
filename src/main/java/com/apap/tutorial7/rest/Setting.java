ab1b249e19c5284e3ba0eee9978a3d642dcb5412
faisal ridwan <faisal.ridwan55@gmail.com>
2018-10-31 19:51:23 +0700

    finish latihan no.2 w/ additional http headers to prevent 403 forbidden access

--- src/main/java/com/apap/tutorial7/rest/Setting.java
+++ src/main/java/com/apap/tutorial7/rest/Setting.java
@@ -2,4 +2,5 @@ package com.apap.tutorial7.rest;
 
 public class Setting {
     final public static String dealerUrl = "https://680355c8-3184-4756-9ce1-eb8a5f8c5f16.mock.pstmn.io";
+    final public static String carQueryUrl = "https://www.carqueryapi.com/api/0.3";
 }
